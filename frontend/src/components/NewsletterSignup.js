72e60d87e92a485fc0f1699f269ed63f584ef235
Theo Everaere <theo-everaere@hotmail.fr>
2023-02-20 20:32:19 -0500

    initial commit

---
+++ frontend/src/components/NewsletterSignup.js
@@ -0,0 +1,36 @@
+import { useEffect } from "react";
+import { useFetcher } from "react-router-dom";
+
+import classes from "./NewsletterSignup.module.css";
+
+function NewsletterSignup() {
+  // useFetcher is basically the tool to use if we want to interact with some action
+  // or loader without transitioning.
+  // If we want to send requests behind the scenes, without triggering any route changes.
+  const fetcher = useFetcher();
+  const { data, state } = fetcher;
+  console.log(data);
+
+  useEffect(() => {
+    if (state === "idle" && data && data.message) {
+      window.alert(data.message);
+    }
+  }, [data, state]);
+
+  return (
+    <fetcher.Form
+      method="post"
+      action="/newsletter"
+      className={classes.newsletter}
+    >
+      <input
+        type="email"
+        placeholder="Sign up for newsletter..."
+        aria-label="Sign up for newsletter"
+      />
+      <button>Sign up</button>
+    </fetcher.Form>
+  );
+}
+
+export default NewsletterSignup;
